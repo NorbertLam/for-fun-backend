cdcc4e53c02c1c36ea47aff0eed77a8d2642ae98
NorbertLam <noobertlm@gmail.com>
2019-07-11 21:57:33 -0400

    db connection

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const {Client} =require('pg');
+
 const app = express();
+const client = new Client({
+  user: 'placeholder',
+  password: 'placeholder',
+  host: 'placeholder',
+  port: 'placeholder',
+  database: 'placeholder'
+})
+
+client.connect()
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch(e => {
+    console.log(e);
+  })
+  .finally(() => {
+    client.end();
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello route')
